423803c4a9a2ecff4ca3f8deb56fdfe17e077e6d
Student-Aaditya <aadityakuamr518@gmail.com>
2025-10-06 21:29:13 +0530

    Uploading task files

---
+++ Routes/super_admin.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const superAdminController = require("../Controller/super_admin.Controller.js");
+
+const auth = require("../Middleware/authMiddeware.js");
+const allowRoles = require("../Middleware/roleMiddleware.js");
+
+router.post("/superAdmin/create", superAdminController.createSuperAdmin);
+router.post("/api/admins", auth, allowRoles("super_admin"), superAdminController.registerAdmin);
+router.get("/api/admins", auth, allowRoles("super_admin"), superAdminController.adminList);
+router.delete("/api/admins/:id", auth, allowRoles("super_admin"), superAdminController.deleteAdmin);
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const app=express();
+const port=7069;
+const admin=require("./Routes/adminRoutes.js");
+const super_admin=require("./Routes/super_admin.routes.js");
+const vendor=require("./Routes/vendorRoutes.js");
+const manager=require("./Routes/managerRoutes.js");
+const bodyparser=require("body-parser");
+const login=require("./Routes/loginRoutes.js");
+app.use(bodyparser.json());
+app.use(express.json());
+
+app.use("/",login);
+app.use("/admin",admin);
+app.use("/superAdmin",super_admin);
+app.use("/vendor",vendor);
+app.use("/manager",manager);
+
+
+app.listen(port,()=>{
+    console.log(`server working on${port}`);
+})
